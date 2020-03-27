081c1e4552211d15f7976ada8fe8d9525e25348c
Guo Lidong <guolidong@genomics.cn>
2020-03-27 15:42:34 +0800

    fixed bug

--- trioSLR.sh
+++ trioSLR.sh
@@ -105,6 +105,10 @@ do
             FILIAL=$2" "$FILIAL
             shift 
             ;;
+        *)
+            echo "invalid params : \"$1\" . exit ... "
+            exit
+        ;;
     esac
     shift
 done
