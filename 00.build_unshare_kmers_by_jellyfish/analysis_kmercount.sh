8284e9475c59b07f6fd0963f1c3ba6687b5dea37
Guo Lidong <guolidong@genomics.cn>
2020-06-18 10:40:26 +0800

    update analysis_kmercount.sh

--- 00.build_unshare_kmers_by_jellyfish/analysis_kmercount.sh
+++ 00.build_unshare_kmers_by_jellyfish/analysis_kmercount.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 path=`dirname $0`
-
+path=`realpath $path`
+JELLY=$path"/jellyfish-linux"
 # calculate kmercount-count table and ordered print it for maternal mers
-awk -F '>' '{if(NF>1) a[$2]=a[$2]+1;} END{for(item in a){printf("%d %d\n",item,a[item]);}}' maternal.mer.fa| sort -n >maternal.kmercount.count.txt
+$JELLY histo -o maternal.histo maternal_mer_counts.jf
 # calculate kmercount-count table and ordered print it for paternal mers
-awk -F '>' '{if(NF>1) a[$2]=a[$2]+1;} END{for(item in a){printf("%d %d\n",item,a[item]);}}' paternal.mer.fa| sort -n >paternal.kmercount.count.txt
+$JELLY histo -o maternal.histo maternal_mer_counts.jf
 
-awk -f $path"/find_bounds.awk" maternal.kmercount.count.txt > maternal.bounds.txt
-awk -f $path"/find_bounds.awk" paternal.kmercount.count.txt > paternal.bounds.txt
+awk -f $path"/find_bounds.awk" maternal.histo > maternal.bounds.txt
+awk -f $path"/find_bounds.awk" paternal.histo > paternal.bounds.txt
 
