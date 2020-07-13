e5b9f5c374b9af002b1a6a537da2707d4c92b65d
Mengyang Xu <xumengyang@genomics.cn>
2020-07-13 08:55:39 +0800

    Update analysis_kmercount.sh

--- 00.build_unshare_kmers_by_jellyfish/analysis_kmercount.sh
+++ 00.build_unshare_kmers_by_jellyfish/analysis_kmercount.sh
@@ -6,7 +6,7 @@ JELLY=$path"/jellyfish-linux"
 # calculate kmercount-count table and ordered print it for maternal mers
 $JELLY histo -o maternal.histo maternal_mer_counts.jf
 # calculate kmercount-count table and ordered print it for paternal mers
-$JELLY histo -o paternal.histo pternal_mer_counts.jf
+$JELLY histo -o paternal.histo paternal_mer_counts.jf
 
 awk -f $path"/find_bounds.awk" maternal.histo > maternal.bounds.txt
 awk -f $path"/find_bounds.awk" paternal.histo > paternal.bounds.txt
