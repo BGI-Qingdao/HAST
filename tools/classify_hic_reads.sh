8f8f16b32fdc3a3c05e2f54ac51535136843feca
Lidong <guolidong@genomics.cn>
2020-10-12 10:32:31 +0800

    updete to remove sort operate

--- tools/classify_hic_reads.sh
+++ tools/classify_hic_reads.sh
@@ -20,8 +20,11 @@ function get_infos(){
     #     chomp ;
     #     @a=split;
     #     print($a[0],"\t",$a[1],"\t",1-($n-$g+$o)/($m+$o) ,"\t",($m+$o)-($n-$g+$o),  "\t",($m+$o),"\n")
+    #}
+    #else{
+    #     print($a[0],"\t0\t0\t0\t0\n")
     #}'
-    cut -f 1,2,6,12 $1 | perl -ane  'if(/NM:i:(\d+)/){ $n=$1;$m=$g=$o=0;$m+=$1 while/(\d+)M/g;$g+=$1,++$o while/(\d+)[ID]/g; chomp ;@a=split;print($a[0],"\t",$a[1],"\t",1-($n-$g+$o)/($m+$o) ,"\t",($m+$o)-($n-$g+$o),  "\t",($m+$o),"\n")}'
+    grep -v "^@" $1 | cut -f 1,2,6,12 | perl -ane  'if(/NM:i:(\d+)/){ $n=$1;$m=$g=$o=0;$m+=$1 while/(\d+)M/g;$g+=$1,++$o while/(\d+)[ID]/g; chomp ;@a=split;print($a[0],"\t",$a[1],"\t",1-($n-$g+$o)/($m+$o) ,"\t",($m+$o)-($n-$g+$o),  "\t",($m+$o),"\n")} else{print($a[0],"\t0\t0\t0\t0\n")} '
 }
 
 function get_scores(){
@@ -39,14 +42,14 @@ function get_scores(){
     #            score=0;
     #        }
     #        name=n;
-    #        if($2<256)
+    #        if($2<256 && $2>0)
     #        {
     #           score+=( 3*(log($3)/log(10)) + (log($5)/log(10)) );
     #        }
     #    }
     #}' $1
 
-    awk 'BEGIN{name="";score=0;}{ if(NR>1){ n=$1; if(n!=name && name!=""){printf("%s\t%f\n",name,score);score=0;} name=n;if($2<256){score+=(3*(log($3)/log(10))+(log($5)/log(10)) );} } }' $1
+    awk 'BEGIN{name="";score=0;}{ if(NR>1){ n=$1; if(n!=name && name!=""){printf("%s\t%f\n",name,score);score=0;} name=n;if($2<256&& $2>0){score+=(3*(log($3)/log(10))+(log($5)/log(10)) );} } }' $1
 }
 
 get_infos $R1SAM >r1.info
@@ -55,10 +58,10 @@ get_infos $R2SAM >r2.info
 get_scores r1.info >r1.scores
 get_scores r2.info >r2.scores
 
-sort -k 1b,1 r1.scores >r1.sort.scores
-sort -k 1b,1 r2.scores >r2.sort.scores
-join -o 1.1 2.1 1.2  2.2  -e 0 -a1 -a2 r1.sort.scores r2.sort.scores  >read.scores 2>join.err
-
+#sort -k 1b,1 r1.scores >r1.sort.scores
+#sort -k 1b,1 r2.scores >r2.sort.scores
+#join -o 1.1 2.1 1.2  2.2  -e 0 -a1 -a2 r1.sort.scores r2.sort.scores  >read.scores 2>join.err
+join -o 1.1 2.1 1.2  2.2  -e 0 -a1 -a2 r1.scores r2.scores  >read.scores 2>join.err
 #{
 #   if($1==0)
 #        name =$2;
